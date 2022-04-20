e54e8f0f10e8491dc8a8f5ff654f3219d7b066b1
usmangq12 <usmangq12@gmail.com>
2022-04-20 16:58:59 +0500

    Created Product Details page

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,6 +1,23 @@
+import { useState, useEffect } from "react";
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import axios from "axios";
 
 export function ProductDetails() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://backendapi.turing.com/products").then((res) => {
+      setProducts(res.data.rows);
+    });
+  }, []);
+
+  console.log(products);
+
   return (
     <>
       <Box sx={{ display: "flex", flexWrap: "wrap" }}>
@@ -12,9 +29,81 @@ export function ProductDetails() {
         />
 
         <Box
-          sx={{ border: "1px solid black", marginLeft: "3rem", width: "55%" }}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            border: "1px solid black",
+            marginLeft: "3rem",
+            width: "55%",
+          }}
         >
-          Product Specifications
+          <Typography
+            sx={{
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+              marginLeft: "2rem",
+              marginBottom: "2rem",
+            }}
+          >
+            Title: Clarks Men's Tilden Cap Oxford Shoe
+          </Typography>
+          <Typography
+            sx={{ fontSize: "1.5rem", fontWeight: "bold", marginLeft: "2rem" }}
+          >
+            Product Features:
+          </Typography>
+
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText
+                  inset
+                  primary="100% Leather
+"
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText inset primary="Imported" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText
+                  inset
+                  primary="Thermoplastic Elastomers sole
+"
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText
+                  inset
+                  primary="Heel measures approximately 0.98"
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText
+                  inset
+                  primary="These smart men's shoes with a square cap toe toe are crafted from a premium leather
+                  "
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText
+                  inset
+                  primary="Ortholite footbed that softens impact and wicks away moisture
+                  "
+                />
+              </ListItemButton>
+            </ListItem>
+          </List>
         </Box>
 
         <Box
