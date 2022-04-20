c0f4298b6110ee4d85408214c7a580c638f6b0fd
usmangq12 <usmangq12@gmail.com>
2022-04-20 16:59:20 +0500

    Manage Routing at Main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
+import { ShippingDetails } from "./components/Shipping Details";
 
 function App() {
   return (
-    
-    <NavBar />
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<NavBar />} />
+        <Route path="shippingdetails" element={<ShippingDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
