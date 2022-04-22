2a2e6d986fc7a78b33cccb512a9f34731c374481
usmangq12 <usmangq12@gmail.com>
2022-04-22 17:09:52 +0500

    Fetched different products api

--- src/components/shopping redux/ServiceApi.js
+++ src/components/shopping redux/ServiceApi.js
@@ -7,7 +7,99 @@ export function ServiceApi() {
         `
     )
     .then((response) => {
+      // console.log(response.data.rows);
+      return response.data.rows;
+    });
+}
+
+export function GetProducts() {
+  return axios
+    .get(
+      `https://backendapi.turing.com/products/inCategory/1?limit=50
+        `
+    )
+    .then((response) => {
       console.log(response.data.rows);
       return response.data.rows;
     });
 }
+
+export function GetProductsItalian() {
+  return axios
+    .get(
+      `https://backendapi.turing.com/products/inCategory/2?limit=50
+
+        `
+    )
+    .then((response) => {
+      console.log(response.data.rows);
+      return response.data.rows;
+    });
+}
+
+export function GetProductsIRISH() {
+  return axios
+    .get(
+      `https://backendapi.turing.com/products/inCategory/3?limit=50
+
+        `
+    )
+    .then((response) => {
+      console.log(response.data.rows);
+      return response.data.rows;
+    });
+}
+
+export function GetProductsAnimal() {
+  return axios
+    .get(
+      `https://backendapi.turing.com/products/inCategory/4?limit=50
+
+        `
+    )
+    .then((response) => {
+      console.log(response.data.rows);
+      return response.data.rows;
+    });
+}
+
+export function GetProductsFlower() {
+  return axios
+    .get(
+      `https://backendapi.turing.com/products/inCategory/5?limit=50
+
+        `
+    )
+    .then((response) => {
+      console.log(response.data.rows);
+      return response.data.rows;
+    });
+}
+
+export function GetProductsChristmas() {
+  return axios
+    .get(
+      `https://backendapi.turing.com/products/inCategory/6?limit=50
+
+        `
+    )
+    .then((response) => {
+      console.log(response.data.rows);
+      return response.data.rows;
+    });
+}
+
+export function GetProductsValentines() {
+  return axios
+    .get(
+      `https://backendapi.turing.com/products/inCategory/7?limit=50
+
+        `
+    )
+    .then((response) => {
+      console.log(response.data.rows);
+      return response.data.rows;
+    });
+}
+
+
