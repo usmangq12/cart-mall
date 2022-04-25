9a40b78bd1f23c7d478414ed1c1ebf264adc3fc9
usmangq12 <usmangq12@gmail.com>
2022-04-25 17:10:23 +0500

    Set routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { ShippingDetails } from "./components/Shipping Details";
+import { ProductDetails } from "./components/ProductDetails";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<NavBar />} />
         <Route path="shippingdetails" element={<ShippingDetails />} />
+        <Route path="productdetails/:id" element={<ProductDetails />} />
       </Routes>
     </Router>
   );
